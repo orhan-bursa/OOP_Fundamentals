8ca6efe577ce9ae6e84dbea2c19dc3786f03421f
Orhan <Orhan@DESKTOP-0GDPOHV>
2022-04-12 11:06:46 +0300

    Add project files.

---
+++ Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Class :
+             * Classes are our prototypes that we use to create objects. It could be thought of as a blueprint 
+             * of a product that determines it's basic structure and properties.
+             * We use classes to create an instance of the object that the class describes
+             * if mobile phone blueprint = class,
+             *    mobile phone instance = mobilephone v.1, we use this instance in our project to get or set 
+             *    it's specified properties, and use it's methods to take certain actions.
+             */
+
+        }
+        //This is the simplest decleration of a class which contains the keyword 'class' followed up with the 
+        //name that represents that class (the Identifier - in this case 'MobilePhone')
+        class MobilePhone 
+        {
+            /* Body of the class :
+             * 
+             */
+        }
+    }
+}
+
